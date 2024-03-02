074b931eb378bf2fe7b12487ca8116b45c760b0f
andresdiaz-97 <andres.diaz1508@gmail.com>
2024-03-01 23:53:10 -0300

    section 1 problem 4 solved

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,11 @@
-function countOccurrences() {}
+function countOccurrences(string, char) {
+  let result = 0
+  for (i = 0; i < string.length; i++) {
+    if (string[i] === char) {
+      result++
+    }
+  }
+  return result
+}
 
-module.exports = countOccurrences;
+module.exports = countOccurrences
