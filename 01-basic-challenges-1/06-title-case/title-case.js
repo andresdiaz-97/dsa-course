9c78ad8627b59fdba0f667078ffb14f59b28f5ad
andresdiaz-97 <andres.diaz1508@gmail.com>
2024-03-20 20:34:32 -0300

    section 1 problem 6 solved, solution pending on check with the course

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,23 @@
-function titleCase() {}
+function titleCase(string) {
+  let result = ""
+  let mayus = true
+  for (i = 0; i < string.length; i++) {
+    console.log(string[i].toUpperCase())
+    if (i === 0) {
+      result += string[i].toUpperCase()
+      mayus = false
+    } else if (mayus === false && string[i] !== " ") {
+      result += string[i].toLowerCase()
+    } else if (mayus === true) {
+      result += string[i].toUpperCase()
+      mayus = false
+    } else if (string[i] === " ") {
+      mayus = true
+      result += string[i].toUpperCase()
+    }
+  }
+  console.log(`this is a result ${result}`)
+  return result
+}
 
-module.exports = titleCase;
+module.exports = titleCase
